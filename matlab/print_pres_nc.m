2510a4313cdda53faf15519eb38c2d6710c5e257
Takuya Miyashita <miyashita@hydrocoast.jp>
2023-11-07 18:09:56 +0900

    update print nc forcing

---
+++ matlab/print_pres_nc.m
@@ -0,0 +1,52 @@
+clear
+close all
+
+%% 作成した気圧データをGeoClawで計算するためのテキストファイルに出力
+
+%% filename
+% matfile = 'pres_lamb.mat';
+matfile = 'pres_lg_A.mat';
+% matfile = 'pres_lg_B.mat';
+load(matfile)
+
+%% output
+ncdir = './slp_nc';
+if exist(ncdir,'dir'); system(['rm -rf  ', ncdir]); end
+mkdir(ncdir);
+
+ncfile_base = 'slp_XXX.nc';
+
+nt = length(t);
+
+% for k = 1:3
+for k = 1:nt
+    ncfile = fullfile(ncdir,strrep(ncfile_base,'XXX',sprintf('%04d',k)));
+    disp([ncfile, '   ...']);
+
+    % % lonlat
+    nccreate(ncfile,'lon',"Dimensions",{"lon",nlon},"FillValue","disable","Datatype", "single");
+    nccreate(ncfile,'lat',"Dimensions",{"lat",nlat},"FillValue","disable","Datatype", "single");
+    ncwrite(ncfile,'lon',lon);
+    ncwrite(ncfile,'lat',lat);
+    % % pressure
+    nccreate(ncfile,'slp',"Dimensions",{"lon",nlon,"lat",nlat},"FillValue","disable","Datatype", "single");
+    ncwrite(ncfile,'slp',permute(flipud(pres(:,:,k)),[2,1]));
+    % % time
+    nccreate(ncfile,'time',"Datatype", "single");
+    ncwrite(ncfile,'time',t(k));
+end
+
+%% single file
+% ncfile = 'slp_all.nc';
+% 
+% % % lonlat
+% nccreate(ncfile,'lon',"Dimensions",{"lon",nlon},"FillValue","disable","Datatype", "single");
+% nccreate(ncfile,'lat',"Dimensions",{"lat",nlat},"FillValue","disable","Datatype", "single");
+% ncwrite(ncfile,'lon',lon);
+% ncwrite(ncfile,'lat',lat);
+% % % time
+% nccreate(ncfile,'time',"Dimensions",{"time",nt},"FillValue","disable","Datatype", "single");
+% ncwrite(ncfile,'time',t);
+% % % pressure
+% nccreate(ncfile,'slp',"Dimensions",{"lon",nlon,"lat",nlat,"time",nt},"FillValue","disable","Datatype", "single");
+% ncwrite(ncfile,'slp',permute(flipud(pres(:,:,:)),[2,1,3]));
