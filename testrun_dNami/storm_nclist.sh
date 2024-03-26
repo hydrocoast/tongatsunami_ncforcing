aee2badb274aa5d3edbf5cec028ded89caacc3c7
Takuya Miyashita <miyashita@hydrocoast.jp>
2024-03-26 21:17:07 +0900

    update sh

--- testrun_dNami/storm_nclist.sh
+++ testrun_dNami/storm_nclist.sh
@@ -2,6 +2,8 @@
 
 fnameout='storm_list.data'
 dir_current=`pwd`
+dir_nc="nc_dNami"
+
 
 echo '# ========================================================= # ' > $fnameout
 echo '# LIST OF STORM DATA FILES                                  # ' >> $fnameout
@@ -9,7 +11,7 @@ echo '# Atmospheric data in NetCDF format                         # ' >> $fnameo
 echo '# ========================================================= # ' >> $fnameout
 echo ' ' >> $fnameout
 echo '300.0              =: dt_file' >> $fnameout
-echo $(ls -1 ../dNami_slp/*.nc | wc -l) >> $fnameout
-ls -1 ../dNami_slp/*.nc | awk -v cdir=`pwd` '{printf "%s/%s\n", cdir, $1}' >> $fnameout
+echo $(ls -1 ../${dir_nc}/*.nc | wc -l) >> $fnameout
+ls -1 ../${dir_nc}/*.nc | awk -v cdir=`pwd` '{printf "%s/%s\n", cdir, $1}' >> $fnameout
 echo "$fnameout has been made"
 
