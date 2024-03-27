0797699fd10c3a2b1ee6e694cae7f9b49697c38e
Takuya Miyashita <miyashita@hydrocoast.jp>
2024-03-27 17:28:28 +0900

    add a run for Amami Islands

--- run_AmamiIslands_presA/storm_nclist_presA.sh
+++ run_AmamiIslands_presA/storm_nclist_presA.sh
@@ -2,16 +2,14 @@
 
 fnameout='storm_list.data'
 dir_current=`pwd`
-dir_nc="nc_dNami"
-
 
 echo '# ========================================================= # ' > $fnameout
 echo '# LIST OF STORM DATA FILES                                  # ' >> $fnameout
 echo '# Atmospheric data in NetCDF format                         # ' >> $fnameout
 echo '# ========================================================= # ' >> $fnameout
 echo ' ' >> $fnameout
-echo '300.0              =: dt_file' >> $fnameout
-echo $(ls -1 ../${dir_nc}/*.nc | wc -l) >> $fnameout
-ls -1 ../${dir_nc}/*.nc | awk -v cdir=`pwd` '{printf "%s/%s\n", cdir, $1}' >> $fnameout
+echo 120.0 >> $fnameout
+echo $(ls -1 ../slp_nc_presA/*.nc | wc -l) >> $fnameout
+ls -1 ../slp_nc_presA/*.nc | awk -v cdir=`pwd` '{printf "%s/%s\n", cdir, $1}' >> $fnameout
 echo "$fnameout has been made"
 
