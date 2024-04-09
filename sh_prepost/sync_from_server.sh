d466c3fa9d55afd0bc51293376131e76229f372f
Takuya Miyashita <miyashita@hydrocoast.jp>
2024-04-10 04:07:48 +0900

    fix sync

--- sh_prepost/sync_from_server.sh
+++ sh_prepost/sync_from_server.sh
@@ -5,5 +5,5 @@ simdir=`basename $(pwd)`
 rsync -av "miyashita@$hostname:Research/AMR/tongatsunami_ncforcing/$simdir/_output/gauge*.txt" _output/
 rsync -av "miyashita@$hostname:Research/AMR/tongatsunami_ncforcing/$simdir/_plots" .
 rsync -av "miyashita@$hostname:Research/AMR/tongatsunami_ncforcing/$simdir/_mat" .
-rsync -av "miyashita@$hostname:Research/AMR/tongatsunami_ncforcing/$simdir/_grd/fg*_max.grd" .
-rsync -av "miyashita@$hostname:Research/AMR/tongatsunami_ncforcing/$simdir/_grd/fgout0002_0*.grd" .
+rsync -av "miyashita@$hostname:Research/AMR/tongatsunami_ncforcing/$simdir/_grd/fg*_max.grd" _grd/
+rsync -av "miyashita@$hostname:Research/AMR/tongatsunami_ncforcing/$simdir/_grd/fgout0002_0*.grd" _grd/
