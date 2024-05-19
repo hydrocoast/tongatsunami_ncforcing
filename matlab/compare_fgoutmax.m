973b31194df70cf577d7db9a7505efadd816e97b
Takuya Miyashita <miyashita@hydrocoast.jp>
2024-05-20 02:32:25 +0900

    delete old runs

--- matlab/compare_fgoutmax.m
+++ matlab/compare_fgoutmax.m
@@ -1,10 +1,24 @@
 clear
 close all
 
-% fname1 = '../run_presA1min_3/_grd/fg0003_max.grd';
-% fname2 = '../run_presA1min_regionC/_grd/fg0003_max.grd';
-fname1 = '../run_presA1min_3/_grd/fg0002_max.grd';
-fname2 = '../run_presA1min_regionC/_grd/fg0002_max.grd';
+% % ---------------------------------
+% fname1 = '../run_presA1min_regionA_fg/_grd/fg0002_max.grd';
+% fname2 = '../run_presA1min_regionC_fg/_grd/fg0002_max.grd';
+% outgrdname = 'zratioAC_fg02.grd';
+% % ---------------------------------
+% fname1 = '../run_presA1min_regionA_fg/_grd/fg0003_max.grd';
+% fname2 = '../run_presA1min_regionC_fg/_grd/fg0003_max.grd';
+% outgrdname = 'zratioAC_fg03.grd';
+% % ---------------------------------
+% fname1 = '../run_presA1min_regionA_fg/_grd/fg0002_max.grd';
+% fname2 = '../run_presA1min_regionB_fg/_grd/fg0002_max.grd';
+% outgrdname = 'zratioAB_fg02.grd';
+% % ---------------------------------
+fname1 = '../run_presA1min_regionA_fg/_grd/fg0003_max.grd';
+fname2 = '../run_presA1min_regionB_fg/_grd/fg0003_max.grd';
+outgrdname = 'zratioAB_fg03.grd';
+% % ---------------------------------
+
 
 [x,y,zmax1] = grdread2(fname1); zmax1(abs(zmax1)<0.5) = NaN;
 [~,~,zmax2] = grdread2(fname2); zmax2(abs(zmax2)<0.5) = NaN;
@@ -44,16 +58,16 @@ tile.TileSpacing = "tight";
 
 zratio = 100*(zmax1-zmax2)./zmax2;
 
-grdwrite2(x,y,zratio,'zratio3.grd');
+grdwrite2(x,y,zratio,outgrdname);
 
 
-bwr16 = createcolormap(16,[0,0,1;1,1,1;1,0,0]);
+bwr20 = createcolormap(20,[0,0,1;1,1,1;1,0,0]);
 
 figure;
 pcolor(x,y,zratio); shading flat
 axis equal tight;
 colorbar;
 
-clim([-80,80]);
-colormap(bwr16);
+clim([-50,50]);
+colormap(bwr20);
 
