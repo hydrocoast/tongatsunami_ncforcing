77c2421979ad4abc8b61310deab7f882bc3d4874
Takuya Miyashita <miyashita@hydrocoast.jp>
2024-05-27 17:41:23 +0900

    add runs for different resolutions

---
+++ run_presA30arcmin_regionC/storm_nclist_presA30arc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+fnameout='storm_list.data'
+dir_current=`pwd`
+dir_nc="nc_presA_30arcmin"
+dt_file=60.0
+
+echo '# ========================================================= # ' > $fnameout
+echo '# LIST OF STORM DATA FILES                                  # ' >> $fnameout
+echo '# Atmospheric data in NetCDF format                         # ' >> $fnameout
+echo '# ========================================================= # ' >> $fnameout
+echo ' ' >> $fnameout
+echo "$dt_file           =: dt_file" >> $fnameout
+echo $(ls -1 ../${dir_nc}/*.nc | wc -l) >> $fnameout
+ls -1 ../${dir_nc}/*.nc | awk -v cdir=`pwd` '{printf "%s/%s\n", cdir, $1}' >> $fnameout
+echo "$fnameout has been made"
+
