e229c2f725f30b94b94324b29ee13a0313575593
Takuya Miyashita <miyashita@hydrocoast.jp>
2024-11-25 06:30:29 +0900

    update

---
+++ matlab/apply_filterpath_patch.m
@@ -0,0 +1,80 @@
+clear
+close all
+
+bwr = createcolormap([0,0,1;1,1,1;1,0,0]);
+
+%% file
+topodir = '../bathtopo';
+file1 = 'gebco_2022_cut.nc';
+file2 = 'gebco_2022_medfilt.nc';
+
+[lon,lat,org] = grdread2(fullfile(topodir,file1));
+[~,~,new] = grdread2(fullfile(topodir,file2));
+
+
+
+fig = figure("Position",[200,500,1000,375]);
+tile = tiledlayout(1,2);
+ax1 = nexttile;
+pcolor(lon,lat,org); shading flat; hold on
+[C,l] = contour(lon,lat,org,-8000:2000:-2000,'w-');
+axis equal tight
+demcmap([-7000,3000]);
+colorbar(ax1);
+
+
+S = contourdata(C);
+
+[~,ind] = maxk([S.numel]',3);
+
+% for i = 1:2
+%     plot(S(ind(i)).xdata, S(ind(i)).ydata, 'm-', 'LineWidth',2);
+% end
+plot(S(ind(1)).xdata, S(ind(1)).ydata, 'm-', 'LineWidth',2);
+plot(S(ind(2)).xdata, S(ind(2)).ydata, 'y-', 'LineWidth',2);
+plot(S(ind(3)).xdata, S(ind(3)).ydata, 'g-', 'LineWidth',2);
+
+[n1,n2,n3] = S(ind).numel;
+[x1,x2,x3] = S(ind).xdata;
+[y1,y2,y3] = S(ind).ydata;
+
+plot(x1(1),y1(1),'mo');
+plot(x2(1),y2(1),'yo');
+plot(x3(1),y3(1),'go');
+
+i1a = 1:5100;
+i1b = 12400:14150;
+i3 = n3:-1:4400;
+i1c = 29800:n1;
+
+xcat = vertcat(max(lon),x1(i1a),x1(i1b),x3(i3),x1(i1c));
+ycat = vertcat(min(lat),y1(i1a),y1(i1b),y3(i3),y1(i1c));
+plot(xcat,ycat,'r-',LineWidth=2);
+
+
+
+lat_above = 16;
+lon_below = 138;
+
+[LON,LAT] = meshgrid(lon,lat);
+TFlon = LON<lon_below;
+TFlat = LAT>lat_above;
+
+in = inpolygon(LON(:),LAT(:),xcat,ycat);
+
+TFapp = in & TFlon & TFlat;
+
+new_filt = org;
+new_filt(TFapp) = new(TFapp);
+
+ax2 = nexttile;
+pcolor(lon,lat,new_filt-org); shading flat
+axis equal tight
+clim(ax2,[-2000,2000])
+colormap(ax2,bwr);
+colorbar(ax2);
+grid on
+box on
+
+
+
--- matlab/filter_bathtopo.m
+++ matlab/filter_bathtopo.m
@@ -7,22 +7,31 @@ bwr = createcolormap([0,0,1;1,1,1;1,0,0]);
 %% file
 topodir = '../bathtopo';
 file1 = 'gebco_2022_cut.nc';
-file2 = 'gebco_2022_medfilt.nc';
+file2 = 'gebco_2022_medfilt2.nc';
 
 [lon,lat,org] = grdread2(fullfile(topodir,file1));
-[~,~,new] = grdread2(fullfile(topodir,file2));
+
+
+filtered1 = medfilt2(org,[50,50]);
+filtered2 = medfilt2(filtered1,[50,50]);
+% filtered1 = medfilt2(org,[100,100]);
+
+
+% grdwrite2(lon,lat,filtered1, fullfile(topodir,file2));
+grdwrite2(lon,lat,filtered2, fullfile(topodir,file2));
+
 
 
 fig = figure("Position",[200,500,1000,375]);
 tile = tiledlayout(1,2);
 ax1 = nexttile;
-pcolor(lon,lat,new); shading flat
+pcolor(lon,lat,filtered2); shading flat
 axis equal tight
 demcmap([-7000,3000]);
 colorbar(ax1);
 
 ax2 = nexttile;
-pcolor(lon,lat,new-org); shading flat
+pcolor(lon,lat,filtered2-org); shading flat
 axis equal tight
 clim(ax2,[-2000,2000])
 colormap(ax2,bwr);
