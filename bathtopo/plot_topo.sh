17b8df03a90c81035a95e1a60ab3e3be4dbdb822
Takuya Miyashita <miyashita@hydrocoast.jp>
2024-11-26 12:50:26 +0900

    newtopo

--- bathtopo/plot_topo.sh
+++ bathtopo/plot_topo.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-topofile="gebco_2022_n60.0_s-60.0_w110.0_e240.0_filter.nc"
+if [ "$#" -lt 1 ]; then
+    echo "usage: $0 [filename]"
+    exit 1
+fi
+
+#topofile="gebco_2022_n60.0_s-60.0_w110.0_e240.0_filter.nc"
+topofile="$1"
 
 gmt begin ${topofile//.nc/} png
     gmt makecpt -Cglobe
-    gmt grdimage -JM10 -R$topofile -C -Baf $topofile
+    gmt grdimage -JM10 -R$topofile -C -Baf $topofile -V
     gmt coast -Di -Wthinnest
 
-    gmt grdimage -JX10d -R120/140/15/35 -C -Baf -BnEwS $topofile -X10.5
+    gmt grdimage -JX10d -R120/140/15/35 -C -Baf -BnEwS $topofile -X10.5 -V
     gmt coast -Di -Wthinnest
 
 gmt end
--- matlab/patch_filtered_bathtopo.m
+++ matlab/patch_filtered_bathtopo.m
@@ -2,9 +2,13 @@ clear
 close all
 
 file_org   = '../bathtopo/gebco_2022_n60.0_s-60.0_w110.0_e240.0.nc';
-file_patch = '../bathtopo/../bathtopo/gebco_2022_filtered.nc';
-
-file_out   = '../bathtopo/gebco_2022_n60.0_s-60.0_w110.0_e240.0_filter.nc';
+% --------------------
+% file_patch = '../bathtopo/gebco_2022_flat_above5000m.nc';
+% file_out   = '../bathtopo/gebco_2022_flat_above5000m_all.nc';
+% --------------------
+file_patch = '../bathtopo/gebco_2022_flat_above5500m.nc';
+file_out   = '../bathtopo/gebco_2022_flat_above5500m_all.nc';
+% --------------------
 
 [lon0,lat0,topo0] = grdread2(file_org);
 [lon1,lat1,topo1] = grdread2(file_patch);
@@ -22,7 +26,7 @@ topo_mod(iy:iy+(ny1-1),ix:ix+(nx1-1)) = topo1;
 
 grdwrite2(lon0,lat0,topo_mod,file_out);
 
-
+% !gmt grdedit -fg $file_out
 
 
 
